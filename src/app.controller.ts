7ab10227ca7aa9ba36924d8a9b739ed104ca63b6
satou-yosiyuki <stuysyk1027@icloud.com>
2022-06-18 00:02:19 +0900

    queryの練習

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// Controller()のカッコの中がパス名
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Get('fuga')
+  getFuga(@Query('text') text): string {
+    return text;
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
